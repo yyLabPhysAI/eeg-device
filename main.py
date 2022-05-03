9db4ed8bafedfc080139767eeb451b26b93b3e40
sabryassaf <91211033+sabryassaf@users.noreply.github.com>
2022-05-03 14:11:35 +0300

    ..

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from matplotlib import pyplot as plt
 from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations
-
-
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
 class Client():
-    def __init__(self):
+    def __init__(self,datatype):
         self.params = BrainFlowInputParams()
         self.params.serial_port = 'com3'
         self.params.board_id = 0
         self.board = BoardShim(0, self.params)
-    #     self.datatype = datatype
+        self.datatype = datatype
+        self.file_path = None
     #     self.matrix = matrix
     #
     # def board_shim(self):
@@ -27,6 +29,20 @@ class Client():
     #     else:
     #         print('hello')
 
+    def collec_data(self,datatype):
+        if datatype == 'real':
+            start_real = Real(self)
+            start_real.collect_data()
+            m = start_real.read_data()
+            for i in range(10):
+                time.sleep(1)
+                d = start_real.read_data()
+                m = np.append(m, d, axis=1)
+            # start_real.stop_stream()
+            return m
+        else:
+            start_fake = Fake(self)
+            file =start_fake.choose_file()
 
 class Real(Client):
     pass
@@ -41,12 +57,21 @@ class Real(Client):
     def stop_stream(self):
         self.board.stop_stream()
         self.board.release_session()
+
+
+class Fake(Client):
+    def choose_file(self):
+        root = tk.Tk()
+        root.withdraw()
+        self.file_path = filedialog.askopenfilename()
+        return self.file_path
+
 def the_data(datatype):
     if datatype == 'real':
         start_real = Real()
         start_real.collect_data()
         m = start_real.read_data()
-        for i in range(10):
+        for i in range(120):
             time.sleep(1)
             d = start_real.read_data()
             m = np.append(m, d, axis=1)
@@ -69,13 +94,19 @@ def the_data(datatype):
 #     matrix = np.append(matrix, data, axis=1)
 # board.stop_stream()
 # board.release_session()
-datatype = 'real'
-matrix = the_data(datatype)
+datatype = 'fake'
+c = Client(datatype)
+matrix = c.collec_data(datatype)
 x = matrix.shape[1] - 1
 # frequency = BoardShim.get_sampling_rate(params.board_id)
 # print(frequency)
-t = np.linspace(0, 10, num=x)
+t = np.linspace(0, 60, num=x)
 test = (matrix[2, 1:]) / 1000000
+# eegmatrix = matrix[0:6, :]
+# transposed = eegmatrix.T
 plt.plot(t, test)
-
+# df = pd.DataFrame(data=transposed)
+#
+# print(df.shape)
+# df.to_csv('outfile.txt', sep=' ', header=False, index=False)
 plt.show()
